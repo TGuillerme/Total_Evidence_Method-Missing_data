63028238cd901249f5e7a1bbe64f3a0ad06ecd5c
TGuillerme <guillert@tcd.ie>
2014-05-16 16:26:13 +0100

    R sourcing function

---
+++ Functions/TreeComparisons/Source.R
@@ -0,0 +1,10 @@
+##########################
+#TreeCmp functions loader
+##########################
+
+#TreeCmp.Read
+source("~/PhD/Projects/Total_Evidence_Method-Missing_data/Functions/TreeComparisons/TreeCmp.Read.R")
+#TreeCmp.Plot
+source("~/PhD/Projects/Total_Evidence_Method-Missing_data/Functions/TreeComparisons/TreeCmp.Plot.R")
+#NTS
+source("~/PhD/Projects/Total_Evidence_Method-Missing_data/Functions/TreeComparisons/NTS.R")
