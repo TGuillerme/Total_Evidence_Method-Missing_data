1e047175916e322ca515aa5ea091fbc1afe8d8c5
TGuillerme <guillert@tcd.ie>
2014-11-28 14:31:25 +0000

    Sub.data can now extract by metric

--- Functions/TreeComparisons/TreeCmp.misc.R
+++ Functions/TreeComparisons/TreeCmp.misc.R
@@ -1,6 +1,17 @@
 #Selecting the given parameter from the data set
-sub.data<-function(data.set, parameter) {
-    sub.data<-data.set[parameter]
-    class(sub.data)<-class(data.set)
-    return(sub.data)
+sub.data<-function(data.set, parameter, metric=NULL) {
+    if(is.null(metric)) {
+        sub.data<-data.set[parameter]
+        class(sub.data)<-class(data.set)
+        return(sub.data)
+    } else {
+        sub.data<-data.set[parameter]
+        metric.column<-grep(metric, colnames(sub.data[[1]]))
+        new.list<-list()
+        for (i in 1:length(sub.data)) {
+            new.list[[i]]<-sub.data[[i]][, metric.column]
+        }
+        names(new.list)<-names(sub.data)
+        return(new.list)        
+    }
 }
