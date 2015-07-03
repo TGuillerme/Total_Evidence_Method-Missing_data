fb4bb78823f7463d85068e0f84e0511d2aeca80f
TGuillerme <guillert@tcd.ie>
2015-07-03 11:50:04 +0100

    Added specific TreeCmp extraction script

---
+++ Functions/Utilities/renaming_TreeCmp.sh
@@ -0,0 +1,28 @@
+#Script for renaming TreeCmp meta files
+
+for n in $(seq -w 01 50)
+do
+	echo Chain${n}:
+
+	#Select the header
+	sed -n '1p' Chain${n}-Bayesian-single-true.Cmp > HEADER
+
+	#Get the total number of lines
+	tot_lines=$(wc -l Chain${n}-Bayesian-single-true.Cmp | sed 's/Chain'"${n}"'-Bayesian-single-true.Cmp//g')
+
+	#Loop through each line to create the file
+	for line in $(seq 2 $tot_lines)
+	do
+		#Select the tree name
+		tree=$(sed -n ''"${line}"'p' Chain${n}-Bayesian-single-true.Cmp | sed 's/Chain'"${n}"'-Truee[[:space:]]Chain'"${n}"'_//g' | sed 's/[[:space:]][0-9].*//g')
+
+		#Creating the new file
+		cat HEADER > Chain${n}_${tree}.Cmp
+		sed -n ''"${line}"'p' Chain${n}-Bayesian-single-true.Cmp | sed 's/Chain'"${n}"'-Truee/1/g' | sed 's/Chain'"${n}"'_'"${tree}"'/1/g' >> Chain${n}_${tree}.Cmp
+		printf .
+	done
+
+	echo "Done."
+done
+
+rm HEADER
