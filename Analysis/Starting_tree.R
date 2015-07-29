4f4afea6204e886984b1cde5df988530619e7ba4
TGuillerme <guillert@tcd.ie>
2015-07-29 17:18:28 +0100

    Starting tree effect (tmp - missing tables)

--- Analysis/Starting_tree.R
+++ Analysis/Starting_tree.R
@@ -1,5 +1,4 @@
 #Script for testing the effect of the starting tree in the Bayesian inference analysis.
-setwd('Analysis/')
 
 #Loading the functions
 sourceDir <- function(path, trace = TRUE, ...) {
@@ -45,17 +44,12 @@ True_to_true<-NTS(tmp, Random51)
 setwd('../../../../Analysis/')
 
 #########################
-#Load the CPU timer
-#########################
-timer<-read.csv("../Data/Starting_tree/timing.csv")
-paste("Using the true tree makes it ", round(sum(timer$random.tree, na.rm=TRUE)/sum(timer$true.tree, na.rm=TRUE), digit=3), " times faster.", sep="")
-
-
-#########################
 #Plotting the differences
 #########################
 
 #Plotting the results (RF)
+quartz(width = 8.3, height = 5.8) #A5 landscape
+op<-par(mfrow=c(1,2), bty="l", oma=c(0,0,0,0))
 TreeCmp.Plot(True_to_best, "R.F_Cluster", probs=c(95, 50), plot=TRUE, col=palette()[1], lines=TRUE, add=FALSE, shift=0, ylim=c(0,1), save.details=FALSE, xaxis='auto', ylab='Normalised Robinson-Foulds metric', xlab=NULL)
 TreeCmp.Plot(Rand_to_best, "R.F_Cluster", probs=c(95, 50), plot=TRUE, col=palette()[2], lines=TRUE, add=TRUE, shift=0.1, ylim=c(0,1), save.details=FALSE, xaxis='auto', ylab='Normalised Robinson-Foulds metric', xlab=NULL)
 TreeCmp.Plot(True_to_true, "R.F_Cluster", probs=c(95, 50), plot=TRUE, col=palette()[3], lines=TRUE, add=TRUE, shift=0.2, ylim=c(0,1), save.details=FALSE, xaxis='auto', ylab='Normalised Robinson-Foulds metric', xlab=NULL)
@@ -68,6 +62,7 @@ TreeCmp.Plot(Rand_to_best, "Triples", probs=c(95, 50), plot=TRUE, col=palette()[
 TreeCmp.Plot(True_to_true, "Triples", probs=c(95, 50), plot=TRUE, col=palette()[3], lines=TRUE, add=TRUE, shift=0.2, ylim=c(-0.5,1), save.details=FALSE, xaxis='auto', ylab='Normalised Triplets metric', xlab=NULL)
 TreeCmp.Plot(Rand_to_true, "Triples", probs=c(95, 50), plot=TRUE, col=palette()[4], lines=TRUE, add=TRUE, shift=0.3, ylim=c(-0.5,1), save.details=FALSE, xaxis='auto', ylab='Normalised Triplets metric', xlab=NULL)
 legend(3.8,1, legend=c("True tree (start) vs best tree","Random tree (start) vs best tree", "True tree (start) vs true tree", "Random tree (start) vs true tree"), col=palette()[1:4], pch=19, cex=0.7)
+par(op)
 
 #########################
 #Measuring the differences (using Bhattacharrya)
@@ -79,60 +74,69 @@ bhatt.coeff(True_to_best$L25F25C25$R.F_Cluster, Rand_to_best$L25F25C25$R.F_Clust
 bhatt.coeff(True_to_best$L50F50C50$R.F_Cluster, Rand_to_best$L50F50C50$R.F_Cluster)
 bhatt.coeff(True_to_best$L75F75C75$R.F_Cluster, Rand_to_best$L75F75C75$R.F_Cluster)
 
-#make a table of the results
-result_table<-timer
-order(result_table, tree)
-result_table$RF<-NA
-result_table$Tr<-NA
-
-
-
-hdr.den(True_to_best$L25F25C25$R.F_Cluster)
-
-
-
-
-#Setting up the parameters
-par_ML<-c(1,26,51,76,101)
-par_MF<-c(1,6,11,16,21)
-par_MC<-c(1:5)
-par_MLMF<-c(1,6,11,16,21, 26,31,36,41,46, 51,56,61,66,71, 76,81,86,91,96, 101,106,111,116,121)
-par_MLMC<-c(1:5, 26:30, 51:55, 76:80, 101:105)
-par_MFMC<-c(1:25)
-par_MLMFMC<-c(1:125)
 
 #########################
-#Comparing the true trees vs the best trees
-#########################
-Best_trees_ML<-ML_besttrees[1] ; class(Best_trees_ML) <- class(ML_besttrees)
-Best_trees_Bayesian<-Bayesian_contrees[1] ; class(Best_trees_Bayesian) <- class(Bayesian_contrees)
-
-op<-par(mfrow=c(2,2),bty="l")
-hdr.den(ML_besttrees[[1]]$R.F_Cluster, prob=c(50, 95), xlab="Normalised Robinson-Foulds metric", main="Maximum Likelihood")
-hdr.den(Best_trees_Bayesian[[1]]$R.F_Cluster, prob=c(50, 95), xlab="Normalised Robinson-Foulds metric", main="Bayesian Inference", ylab="")
-hdr.den(ML_besttrees[[1]]$Triples, prob=c(50, 95), xlab="Normalised Triplets metric", main="")
-hdr.den(Best_trees_Bayesian[[1]]$Triples, prob=c(50, 95), xlab="Normalised Triplets metric", main="", ylab="")
-par(op)
-
-#########################
-#Comparing the true trees vs all the trees
+#Measuring the differences (anova)
 #########################
 
-#data parameters setup
-data.sets<-c("ML_besttrees","Bayesian_contrees")
-metrics<-c("R.F_Cluster","Triples")
+#Preparing the data
+#Comparisons to best tree
+True_RF<-c(True_to_best[[1]][[4]])
+True_Tr<-c(True_to_best[[1]][[6]])
+Rand_RF<-c(Rand_to_best[[1]][[4]], mean(Rand_to_best[[2]][[4]])) #One value is missing
+Rand_Tr<-c(Rand_to_best[[1]][[6]], mean(Rand_to_best[[2]][[6]])) #One value is missing
+for (tree in 2:length(True_to_best)) {
+    True_RF<-c(True_RF, True_to_best[[tree]][[4]])
+    True_Tr<-c(True_Tr, True_to_best[[tree]][[6]])
+    Rand_RF<-c(Rand_RF, Rand_to_best[[tree]][[4]])
+    Rand_Tr<-c(Rand_Tr, Rand_to_best[[tree]][[6]])
+}
+trees<-c(rep("0", 20),rep("10", 20),rep("25", 20),rep("50", 20),rep("75", 20))
+best_RF_test<-data.frame("RF"=c(True_RF, Rand_RF), "starting"=as.factor(c(rep("true", 100), rep("rand", 100))), "miss.data"=rep(trees,2))
+best_Tr_test<-data.frame("Tr"=c(True_Tr, Rand_Tr), "starting"=as.factor(c(rep("true", 100), rep("rand", 100))), "miss.data"=rep(trees,2))
+#Comparisons to best true
 
-global.TreeCmp.plot( data.sets , par_MLMFMC, metrics, col=c('black', 'grey'), las=2, probs=c(95, 50), ylim=c(0,1,-0.5,0.5), xaxis=NULL, las=2, xlab=NULL, shift=0.5, format='A5', col.grad=c("white", "black")) 
+True_RF<-c(True_to_true[[1]][[4]])
+True_Tr<-c(True_to_true[[1]][[6]])
+Rand_RF<-c(Rand_to_true[[1]][[4]], mean(Rand_to_true[[2]][[4]])) #One value is missing
+Rand_Tr<-c(Rand_to_true[[1]][[6]], mean(Rand_to_true[[2]][[6]])) #One value is missing
+for (tree in 2:length(True_to_true)) {
+    True_RF<-c(True_RF, True_to_true[[tree]][[4]])
+    True_Tr<-c(True_Tr, True_to_true[[tree]][[6]])
+    Rand_RF<-c(Rand_RF, Rand_to_true[[tree]][[4]])
+    Rand_Tr<-c(Rand_Tr, Rand_to_true[[tree]][[6]])
+}
+true_RF_test<-data.frame("RF"=c(True_RF, Rand_RF), "starting"=as.factor(c(rep("true", 100), rep("rand", 100))), "miss.data"=rep(trees,2))
+true_Tr_test<-data.frame("Tr"=c(True_Tr, Rand_Tr), "starting"=as.factor(c(rep("true", 100), rep("rand", 100))), "miss.data"=rep(trees,2))
 
-#Why are the Bayesian trees getting better? Maybe because of the level of dissimilarity between the best and the true tree.
-#As the best tree is fairly different from the true tree in RF, when the Bayesian consensus tree loses data, it becomes more of a unresolved tree.
-#Therefore, the difference between the clades in the true tree and the missing data trees (politomies) are more similar than the one between the best tree.
-#One can argue that then polytomical trees are shittier but it's probably wrong: an unresolved node (equivalent to a NA) is better than a wrongly resolved node (equivalent to a false positive)
 
-#The idea is that the way the Bayesian topology is built is fundamently different than the likelihood one.
-#I likelihood, a single topology is calculated: that's the most likely one (i.e. the equation as a finite number of solutions).
-#Then the bootstrap is meant to show a posteriori (and the world is important here) the strength of our hypothesis (i.e. this is the best solution) to bootstrapping (i.e. quicking the data and checking the recovery rate).
-#On the other hand, in Bayesian, a set of good topologies is calculated (i.e. there is an infinite solutions to the equation).
-#Then the topology is built a posteriori (again, I emphasize that, in ML the support for the solution (topology) is built a posteriori (kind of ad hoc) as in Bayesian, solution is build a posteriori (from a distribution of solutions)).
-#This makes it more likely for the ML method to just pick the wrong topology knowing the data (that is more and more wrong when data is lacking) and then to try to support it a posteriori.
-#In the Bayesian case, it is more likely that the topology will be unresolved as the data is lacking and therefore being still robust clade conservation wise (but shit wild taxa wild).
+
+
+
+
+#Running the tests
+library(xtable)
+library(lme4)
+#General effect
+best_RF<-(summary(lm(RF~starting, data=best_RF_test)))
+best_Tr<-(summary(lm(Tr~starting, data=best_Tr_test)))
+true_RF<-(summary(lm(RF~starting, data=true_RF_test)))
+true_Tr<-(summary(lm(Tr~starting, data=true_Tr_test)))
+
+tmp1<-rbind(best_RF[[4]], best_Tr[[4]],true_RF[[4]], true_Tr[[4]])
+comp<-c(rep("best",4),rep("true",4))
+metric<-rep(c(rep("RF",2),rep("Tr",2)),2)
+tmp2<-matrix(data=c(comp, metric, rownames(tmp)), nrow=8, byrow=FALSE)
+tmp3<-cbind(tmp2, tmp1)
+rownames(tmp3)<-NULL ; colnames(tmp3)[1:3]<-c("Comparison", "metric", "term")
+xtable(as.data.frame(tmp3), digit=3) # Digit problem
+
+
+#Nested effect
+xtable(summary(lmList(RF~starting | miss.data, data=best_RF_test))[[4]][,,2])
+xtable(summary(lmList(Tr~starting | miss.data, data=best_Tr_test))[[4]][,,2])
+xtable(summary(lmList(RF~starting | miss.data, data=true_RF_test))[[4]][,,2])
+xtable(summary(lmList(Tr~starting | miss.data, data=true_Tr_test))[[4]][,,2])
+
+
+
